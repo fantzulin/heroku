8ca7927210f8b9860e652145dababe8bce5ff4fe
Chris Fan <fanchi0917@gmail.com>
2021-08-31 11:32:36 +0800

    加上 package.json
    結果 npm start 發現 port 的更改設定無法使用，就暫時復原回原本的樣子測試，然後加上 package.json 來讓專案執行。

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = process.env.PORT || 5000;
+const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -9,4 +9,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello World');
 });
 
-server.listen(PORT);
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
