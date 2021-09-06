a574e7ce8aba4cd3adf0abf1e0b5c584ba413df5
Chris Fan <fanchi0917@gmail.com>
2021-09-06 17:35:42 +0800

    加上 element plus
    套用 element plus 模組，將 home 頁面的架構改寫。

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+  <el-container>
+    <el-header>
+      <el-menu
+        default-active="1"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+        router="true"
+      >
+        <el-menu-item index="1" route="/">Home</el-menu-item>
+        <el-menu-item index="2" route="/about">About</el-menu-item>
+      </el-menu>
+    </el-header>
+    <el-main>
+      <router-view />
+    </el-main>
+  </el-container>
 </template>
 
 <style lang="scss">
@@ -14,17 +26,8 @@
   text-align: center;
   color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
+
+<script>
+
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <el-row :gutter="20">
+    <el-col :span="8">
+      <div class="grid-content">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <h3>FAN TZU-LIN</h3>
+        <h5>Front-End Developer</h5>
+      </div>
+    </el-col>
+    <el-col :span="16">
+      <div class="grid-content">
+        <h5>Skills</h5>
+        <ul>
+          <li>HTML5</li>
+          <li>CSS3</li>
+          <li>JavaScript</li>
+        </ul>
+        <h5>Experience</h5>
+        <h5>Education</h5>
+      </div>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
-export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
 </script>
