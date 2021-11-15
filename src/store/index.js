3f0518794ab4d84385a8033e89a3bca52025186e
Chris Fan <fanchi0917@gmail.com>
2021-11-15 16:23:01 +0800

    initial firebase
    初始化 firebase，目前可以 (set, get, push, update)，怎麼樣好好使用還沒有實際作法...

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,26 @@
 import { createStore } from "vuex";
+import { initializeApp } from "firebase/app";
+import { getDatabase, get, ref } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB900FNxKYIwpFvlFcs4SqZro8bHPiqxvs",
+  authDomain: "fantzulin-e590c.firebaseapp.com",
+  projectId: "fantzulin-e590c",
+  storageBucket: "fantzulin-e590c.appspot.com",
+  messagingSenderId: "937701848678",
+  appId: "1:937701848678:web:d30e705555247cb5b11fa7",
+};
+const app = initializeApp(firebaseConfig);
+
+function writeUserData() {
+  const db = getDatabase(app);
+
+  get(ref(db, '/food')).then(function (snapshot) {
+    console.log(snapshot.val());
+  }).catch(function () {
+    alert("伺服器發生錯誤，請稍後再試");
+  });
+}
 
 export default createStore({
   state: {},
@@ -6,3 +28,4 @@ export default createStore({
   actions: {},
   modules: {},
 });
+writeUserData();
