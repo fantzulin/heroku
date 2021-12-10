fd279e7cf284d7b492fd3924a356532d6f501c51
Chris Fan <fanchi0917@gmail.com>
2021-12-10 14:20:22 +0800

    修改 firebase 的安全性
    firebase 被警告有安全性的疑慮，依照官方文件將 write 設為 false

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,30 @@
 import { createStore } from "vuex";
 import { initializeApp } from "firebase/app";
-import { getDatabase, get, ref } from "firebase/database";
+import { getDatabase, ref, child, get } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB900FNxKYIwpFvlFcs4SqZro8bHPiqxvs",
   authDomain: "fantzulin-e590c.firebaseapp.com",
+  databaseURL: "https://fantzulin-e590c-default-rtdb.firebaseio.com",
   projectId: "fantzulin-e590c",
   storageBucket: "fantzulin-e590c.appspot.com",
   messagingSenderId: "937701848678",
   appId: "1:937701848678:web:d30e705555247cb5b11fa7",
+  measurementId: "${config.measurementId}"
 };
+
 const app = initializeApp(firebaseConfig);
 
 function writeUserData() {
-  const db = getDatabase(app);
-
-  get(ref(db, '/food')).then(function (snapshot) {
-    console.log(snapshot.val());
-  }).catch(function () {
-    alert("伺服器發生錯誤，請稍後再試");
+  const dbRef = ref(getDatabase(app));
+  get(child(dbRef, '/food')).then((snapshot) => {
+    if (snapshot.exists()) {
+      console.log(snapshot.val());
+    } else {
+      console.log("No data available");
+    }
+  }).catch((error) => {
+    console.error(error);
   });
 }
 
