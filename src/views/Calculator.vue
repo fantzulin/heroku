e7812963a0edc5594f0bdeae7144347a62b7a79e
Chris Fan <fanchi0917@gmail.com>
2022-04-26 17:53:48 +0800

    feat: add ethers.js, opensea api
    將NFT成本計算器串接Api作查詢，目前用交易的id可查詢 mint 價以及其他相關費用。
    取得合約後，串接opensea拿到版稅做計算。
    bug: 目前有同異步的問題，導致時間差影響程式前後順序。

--- src/views/Calculator.vue
+++ src/views/Calculator.vue
@@ -2,15 +2,27 @@
   <div class="max-w-half mx-auto text-start">
     <h3>計算 NFT 成本</h3>
     <el-row :gutter="20" class="align-items-center">
-      <span>mint 價格（含 gas fee）</span>
-      <el-input class="m-2 max-w-150" v-model="mintPrice" placeholder="請輸入 mint 價格（含 gas fee）" />
+        <span>Transaction Hash</span>
+        <el-input class="m-2 max-w-150" v-model="transactionHash" placeholder="Transaction Hash" />
     </el-row>
+    <div>
+        <p>Value</p>
+        <h5>{{ value }}</h5>
+    </div>
+    <div>
+        <p>Transaction fee</p>
+        <h5>{{ transFee }}</h5>
+    </div>
     <el-row :gutter="20" class="align-items-center">
       <span>OpenSea 上架費用</span>
       <el-input class="m-2 max-w-150" v-model="listPrice" placeholder="請輸入 OpenSea 上架費用" />
     </el-row>
+    <div>
+        <p>{{ nftName }} Royalty</p>
+        <h5></h5>
+    </div>
     <el-row :gutter="20" class="align-items-center">
-      <span>版稅，通常是 7.5%（ Royalty + Platform fee ）</span>
+      <span>Royalty</span>
       <el-input class="m-2 max-w-150" v-model="royaltyPrice" placeholder="請輸入版稅，通常是 7.5%" />
     </el-row>
     <el-button v-on:click="getPrice">計算</el-button>
@@ -50,14 +62,21 @@ input {
 
 <script>
 let math = require('mathjs');
+import { ethers } from "ethers";
+let provider = ethers.getDefaultProvider('homestead');
+const axios = require('axios');
 
 export default {
   data() {
     return {
-      mintPrice: 0,
-      listPrice: 0,
-      royaltyPrice: 0,
-      salePrice:0,
+        transactionHash: '',
+        value: '',
+        transFee: '',
+        nftName: '',
+        contract: '',
+        listPrice: 0,
+        royaltyPrice: 0,
+        salePrice:0,
     }
   },
   methods: {
@@ -74,10 +93,29 @@ export default {
       return comp('divide', arguments)
     },
     getPrice(){
-      let buy_price = math.add(this.mintPrice, this.listPrice);
-      let roy_price = math.divide(this.royaltyPrice, 100);
-      let roy_count = math.subtract(1, roy_price);
-      this.salePrice = math.divide(buy_price, roy_count)
+        let transactionHash = this.transactionHash;
+
+        provider.getTransaction(transactionHash).then((receipt) => {
+            console.log('getTransaction:', receipt);
+            this.value = Number(ethers.utils.formatEther(receipt.value));
+        });
+        provider.getTransactionReceipt(transactionHash).then((receipt) => {
+            console.log('getTransactionReceipt:', receipt);
+            this.transFee = ethers.utils.formatEther(receipt.gasUsed.mul(receipt.effectiveGasPrice));
+            this.contract = receipt.logs[0].address;
+            axios.get('https://api.opensea.io/api/v1/asset_contract/' + this.contract)
+            .then(function (response) {
+                console.log(response)
+                // let dataName = response.data.name;
+                // let basisFee = response.data.seller_fee_basis_points;
+                // let sellerFee = response.data.opensea_seller_fee_basis_points;
+            });
+        });
+
+        let buy_price = math.add(this.value, this.transFee, this.listPrice);
+        let roy_price = math.divide(this.royaltyPrice, 100);
+        let roy_count = math.subtract(1, roy_price);
+        this.salePrice = math.divide(buy_price, roy_count)
     },
   },
 }
