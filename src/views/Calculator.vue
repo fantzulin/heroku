abc687bdd9eedaf9bdbe2a86544b7c7ecaa627cc
Chris Fan <fanchi0917@gmail.com>
2022-04-27 18:00:13 +0800

    feat: 取得資料的順序優化
    目前已經可以依序取得資料，將內容都印在畫面上。
    因為非同步的時間差，需要在取得各項資料之後，最後再來計算成本價，目前我還無法知道所有數值都取到的時間點。

--- src/views/Calculator.vue
+++ src/views/Calculator.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="max-w-half mx-auto text-start">
     <h3>計算 NFT 成本</h3>
-    <el-row :gutter="20" class="align-items-center">
-        <span>Transaction Hash</span>
-        <el-input class="m-2 max-w-150" v-model="transactionHash" placeholder="Transaction Hash" />
-    </el-row>
+    <div>
+        <p>Transaction Hash</p>
+        <el-input class="m-2" v-model="transactionHash" placeholder="Transaction Hash" />
+        <el-button v-on:click="getPrice">計算</el-button>
+    </div>
     <div>
         <p>Value</p>
         <h5>{{ value }}</h5>
@@ -13,19 +14,15 @@
         <p>Transaction fee</p>
         <h5>{{ transFee }}</h5>
     </div>
-    <el-row :gutter="20" class="align-items-center">
-      <span>OpenSea 上架費用</span>
-      <el-input class="m-2 max-w-150" v-model="listPrice" placeholder="請輸入 OpenSea 上架費用" />
-    </el-row>
     <div>
         <p>{{ nftName }} Royalty</p>
-        <h5></h5>
+        <h5>{{ royaltyPrice }}%</h5>
     </div>
     <el-row :gutter="20" class="align-items-center">
-      <span>Royalty</span>
-      <el-input class="m-2 max-w-150" v-model="royaltyPrice" placeholder="請輸入版稅，通常是 7.5%" />
+      <span>OpenSea 上架費用</span>
+      <el-input class="m-2" v-model="listPrice" placeholder="請輸入 OpenSea 上架費用" />
     </el-row>
-    <el-button v-on:click="getPrice">計算</el-button>
+    
     <h4>至少需要賣 {{salePrice}} eth</h4>
   </div>
 </template>
@@ -47,17 +44,10 @@
   max-width: 50%;
 }
 
-.max-w-150 {
-  max-width: 150px;
-}
-
 .text-start {
   text-align: left;
 }
 
-input {
-  max-width: 100px;
-}
 </style>
 
 <script>
@@ -75,7 +65,7 @@ export default {
         nftName: '',
         contract: '',
         listPrice: 0,
-        royaltyPrice: 0,
+        royaltyPrice: '',
         salePrice:0,
     }
   },
@@ -95,27 +85,39 @@ export default {
     getPrice(){
         let transactionHash = this.transactionHash;
 
-        provider.getTransaction(transactionHash).then((receipt) => {
+        let mintValue = provider.getTransaction(transactionHash).then((receipt) => {
             console.log('getTransaction:', receipt);
-            this.value = Number(ethers.utils.formatEther(receipt.value));
-        });
-        provider.getTransactionReceipt(transactionHash).then((receipt) => {
-            console.log('getTransactionReceipt:', receipt);
-            this.transFee = ethers.utils.formatEther(receipt.gasUsed.mul(receipt.effectiveGasPrice));
-            this.contract = receipt.logs[0].address;
-            axios.get('https://api.opensea.io/api/v1/asset_contract/' + this.contract)
-            .then(function (response) {
-                console.log(response)
-                // let dataName = response.data.name;
-                // let basisFee = response.data.seller_fee_basis_points;
-                // let sellerFee = response.data.opensea_seller_fee_basis_points;
-            });
+            return Number(ethers.utils.formatEther(receipt.value));
         });
 
-        let buy_price = math.add(this.value, this.transFee, this.listPrice);
-        let roy_price = math.divide(this.royaltyPrice, 100);
-        let roy_count = math.subtract(1, roy_price);
-        this.salePrice = math.divide(buy_price, roy_count)
+        mintValue.then((successMessage) => {
+            console.log('mintValue success:', successMessage)
+            this.value = successMessage;
+        });
+
+        let transReceipt = provider.getTransactionReceipt(transactionHash).then((receipt) => {
+            let receiptMsg = {
+                transFee: ethers.utils.formatEther(receipt.gasUsed.mul(receipt.effectiveGasPrice)),
+                contract: receipt.logs[0].address,
+            }
+            return receiptMsg
+        });
+
+        transReceipt.then((successMessage) => {
+            let openseaMsg = {};
+            console.log('getTransactionReceipt success:', successMessage)
+            this.transFee = successMessage.transFee;
+            this.contract = successMessage.contract;
+            axios.get('https://api.opensea.io/api/v1/asset_contract/' + successMessage.contract).then((response) => {
+                console.log(response)
+                openseaMsg = {
+                    nftName: response.data.name,
+                    royaltyPrice: math.divide(response.data.seller_fee_basis_points, 100),
+            }
+            this.nftName = openseaMsg.nftName;
+            this.royaltyPrice = openseaMsg.royaltyPrice;
+            });
+        });
     },
   },
 }
@@ -127,4 +129,21 @@ function comp(_func, args) {
   }
   return parseFloat(t.done())
 }
+
+function roundToFour(num) {
+    return +(Math.round(num + "e+4")  + "e-4");
+}
+
+console.log('roundToFour:', roundToFour)
+
+// let x = math.add(mintValue, this.transFee);
+// console.log('value_2:', this.value);
+// console.log("x:", x)
+// let cost = math.add(x, this.listPrice);
+// console.log("cost:", cost)
+// let royPrice = math.divide(response.data.seller_fee_basis_points, 10000);
+// let royPriceNum = math.add(1, royPrice);
+// console.log("royPrice:", royPrice)
+// let y = roundToFour(math.multiply(cost, royPriceNum));
+// this.salePrice = y;
 </script>
